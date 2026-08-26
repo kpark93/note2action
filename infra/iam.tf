6a081e5a4432446ba0c8ae41401738652f224789
Kyle Park <kpstarry@gmail.com>
2026-08-26 12:38:50 -0700

    infra: ecs task-execution role with scoped ssm read

---
+++ infra/iam.tf
@@ -0,0 +1,31 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "task_execution" {
+  name = "${local.name}-task-execution"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_managed" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "task_execution_ssm" {
+  name = "read-note2action-params"
+  role = aws_iam_role.task_execution.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["ssm:GetParameters"]
+      Resource = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter/note2action/*"
+    }]
+  })
+}
